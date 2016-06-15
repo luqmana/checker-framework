e91f37a42d46ab77a6667989fc58139637e8da52
Michael Ernst <mernst@cs.washington.edu>
2016-06-14 18:46:36 -0700

    Try running more tests under Travis

--- .travis-build.sh
+++ .travis-build.sh
@@ -19,22 +19,25 @@ ant javadoc-private
 # The JDK was built above; there is no need to rebuild it again.
 # Don't use "-d" to debug ant, because that results in a log so long
 # that Travis truncates the log and terminates the job.
+
 ## Alternative 1 (desired alternative):
-# ant tests-nobuildjdk
-# (cd checker && ant check-compilermsgs check-purity)
+ant tests-nobuildjdk
+(cd checker && ant check-compilermsgs check-purity check-tutorial)
+
 ## Alternative 2 (because alternative 1 currently crashes);
 ## just run tests that Travis doesn't crash on.
 ## Eventually, we will remove this alternative from the file.
-# Run framework tests.
-(cd framework && ant all-tests-nojtreg-nobuild)
-# Subset of all-tests
-# Also runs framework jtreg-tests
-(cd checker && ant jtreg-tests)
-(cd checker && ant command-line-tests)
-(cd checker && ant example-tests-nobuildjdk)
-(cd checker && ant check-tutorial)
-# If too many checker tests are run, the tests crash, so run one.
-(cd checker && ant nullness-base-tests regex-qual-tests lock-tests lock-safedefaults-tests)
+# # Run framework tests.
+# (cd framework && ant all-tests-nojtreg-nobuild)
+# # Subset of all-tests
+# # Also runs framework jtreg-tests
+# (cd checker && ant jtreg-tests)
+# (cd checker && ant command-line-tests)
+# (cd checker && ant example-tests-nobuildjdk)
+# (cd checker && ant check-tutorial)
+# # If too many checker tests are run, the tests crash, so run one.
+# (cd checker && ant nullness-base-tests regex-qual-tests lock-tests lock-safedefaults-tests)
+
 ## end of alternatives.
 
 # It's cheaper to run the demos test here than to trigger the
